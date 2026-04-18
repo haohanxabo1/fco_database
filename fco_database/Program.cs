f8d88e380c4c828e7d1c0d7224b2b0b97f7d9945
haohanxabo <longnhatleeee02@gmail.com>
2026-04-18 19:04:49 +0200

    Added 3 folder

--- fco_database/Program.cs
+++ fco_database/Program.cs
@@ -11,13 +11,18 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
-
+        
+        // builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseHttpsRedirection();
